1f9d2db92fbc606a2ce98eab4772332b36094710
Canjica <linicker11@gmail.com>
2024-09-11 16:08:20 -0300

    Adicionar marcar/desmarcar itens da lista

---
+++ index.js
@@ -0,0 +1,86 @@
+const {select, input, checkbox} = require('@inquirer/prompts')
+//aqui virá um objeto - require me dara um objeto
+//importando modulos
+let meta = {
+    value: 'Tomar 3L de agua todo dia',
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async ()=>{
+    const meta = await input({ message: "Digite a meta:"})
+
+        if(meta.length == 0){
+            console.log('A meta não pode ser vazia')
+            return
+        }
+
+        metas.push(
+            {value: meta, checked: false}
+        )
+    }
+
+const listarMetas = async() =>{
+    const respostas = await checkbox({
+        message: "Use as setas para mudas de meta, o espaço para marcar e desmarcar e o enter para finalizar essa etapa",
+        choices: [...metas], //... - spread operator -pega tudo que tem e joga no array
+        instructions: false,
+    })
+
+    if (respostas.length == 0){
+        console.log("Nenhuma meta selecionada.")
+        return
+    } 
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+        meta.checked = true
+    })
+
+    console.log ('Meta(s) Concluidas')
+}
+
+const start = async () => {
+
+    while(true){
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+        
+        switch (opcao) {
+            case "cadastrar":
+                await cadastrarMeta()
+                console.log(metas)
+                break
+            case "listar":
+                await listarMetas()
+                break
+            case "sair":
+                return
+        }
+    }
+}
+
+start()
